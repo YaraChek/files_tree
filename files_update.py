b75e9d7d4522d37f76e88c940d4ecc7decce846a
Pro <ahtyrka@gmail.com>
2024-03-12 23:23:36 +0200

    add custom file second

---
+++ files_update.py
@@ -0,0 +1,25 @@
+import os
+
+folder = r"./second_work_directory/"
+
+counter = 0
+# Iterate
+for file in os.listdir(folder):
+	# Rename file with a patterns in name, and replace underscore with a dush
+    suffixName1 = file.replace ("_", "-")
+    suffixName2 = suffixName1.replace ("-patch.yaml", ".yaml")
+    newName = suffixName2.replace (".patch.yaml", ".yaml")
+    counter=counter+1
+    print("{}. old file: {}".format(counter, file))
+    print("{}. new file: {}\n".format(counter, newName))
+
+    # Rename the file
+    fSource = "{}/{}".format(folder, file)
+    # print(fSource)
+    fDestination = "{}/{}".format(folder, newName)
+    # print(fDestination)
+    os.rename(fSource, fDestination)
+    # print(file)
+
+res = os.listdir(folder)
+print(res)
